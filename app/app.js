09863c169b25b5c8584aff4f6ae8a69deaef53b6
Keppelen <keppelen.as@gmail.com>
2012-12-13 00:27:33 -0200

    function login

--- app/app.js
+++ app/app.js
@@ -8,7 +8,20 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
-    }
+    },
+
+      'click #login': function () {
+        alert('Oi eu sou o login');
+      },
+
+       'click #logout': function () {
+        alert('Oi eu sou o logout');
+      },
+
+       'click #home': function () {
+        alert('Oi eu sou o home');
+      },
+
   });
 }
 
